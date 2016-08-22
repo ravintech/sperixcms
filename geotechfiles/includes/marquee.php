78b6bfae59c2f30a06226790e41ca6e7e98b8d43
jayluxferro <justiceowusuagyemang@gmail.com>
2016-08-22 08:19:31 +0000

    adding theme

---
+++ geotechfiles/includes/marquee.php
@@ -0,0 +1,8 @@
+<?php 
+	$theme=$this->getTheme();
+	$configData=$this->getConfigurationData();
+?>
+<div class="row" style="border-radius: 10px; -moz-border-radius: 10px;-webkit-border-radius: 10px; margin: 5px;background-color: #000; color: #fff; font-weight: bold; padding: 0px;">
+<center><h4><marquee behaviour="scroll" direction="left" scrollamount="4" onmouseover="this.stop()" onmouseout="this.start()"><?php echo $configData[1]; ?></marquee></h4></center>
+</div>
+
